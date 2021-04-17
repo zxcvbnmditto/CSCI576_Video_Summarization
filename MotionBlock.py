62a97b6a17ae84e964cd25a3517e995a2ceeec06
MarkStudio <markchen8024@gmail.com>
2021-04-17 11:20:50 +0800

    init code

---
+++ MotionBlock.py
@@ -0,0 +1,8 @@
+class MotionBlock:
+    def __init__(self, data):
+        self.data = data
+
+    def run(self):
+        '''Algorithm Entry'''
+        print("Running MotionBlock")
+        print(self.data)
