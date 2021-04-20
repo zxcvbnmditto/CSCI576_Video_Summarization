566dc64e1c74dc54e3c0e1a580aec57a1abe64e9
MarkStudio <markchen8024@gmail.com>
2021-04-20 23:28:47 +0800

    Add a function to return score array.

--- MotionDetector.py
+++ MotionDetector.py
@@ -1,23 +1,16 @@
 import cv2
-import matplotlib.pyplot as plt
 
 class MotionDetector:
     def __init__(self, data):
         self.data = data
-        self.score = []
 
-    def run(self):
-        start = 0
-        end = 6000
-        step = 5
-        test_frames = []
-        for i in range(int((end-start)/step)):
-            test_frames.append(self.data.load(start+step*i).bgr)
-
+    def get_motion_score(self, start, end, step, threshold):
         last_frame = None
         motion_score = []
-        for frame in test_frames:
-            cv2.imshow('frame', frame)
+
+        for i in range(int((end-start)/step)):
+            frame = self.data.load(start + step*i).bgr
+            # cv2.imshow('frame', frame)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -27,11 +20,11 @@ class MotionDetector:
 
             frame_delta = cv2.absdiff(last_frame, gray)
 
-            thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
-            cv2.imshow('thresh', thresh)
-            cv2.waitKey(0)
+            thresh = cv2.threshold(frame_delta, threshold, 255, cv2.THRESH_BINARY)[1]
+            # cv2.imshow('thresh', thresh)
+            # cv2.waitKey(0)
 
-            thresh = cv2.dilate(thresh, None, iterations=2)
+            thresh = cv2.dilate(thresh, None, iterations=1)
             last_frame = gray
 
             sum = 0
@@ -40,8 +33,6 @@ class MotionDetector:
                     if(thresh[h][w] != 0):
                         sum+=1
 
-
             motion_score.append(sum)
 
-        plt.plot(motion_score)
-        plt.show()
+        return motion_score
