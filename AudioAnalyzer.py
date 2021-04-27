9577492b473089f481932517af1d38b99ea5c473
ruochengW <60168990+ruochengW@users.noreply.github.com>
2021-04-27 09:04:58 +0800

    AudioAnalyzer

--- AudioAnalyzer.py
+++ AudioAnalyzer.py
@@ -1,29 +1,24 @@
+pusg
+
+SAMPLE_PER_FRAME = 640
 class AudioAnalyzer:
-    def __init__(self, breaks, audio_data):
+    def __init__(self, breaks, data):
         self.breaks = breaks
-        self.audio_data= audio_data
-    
+        self.data = data
+        self.raw_data = self.data.raw_data
+
     def get_audio_score(self):
         """
-        return each shots a score
+        Get audio score for each shot
+        return A list of integer represents score of each shot
         """
-        audio_score = []
+        weights = []
         prev_break = 0
         for break_ in self.breaks:
             if break_ == 0:
                 continue
-            shot_score = self._get_score(prev_break, break_)
-            audio_score.append(shot_score)
+            score = sum(self.raw_data[prev_break*SAMPLE_PER_FRAME:break_*SAMPLE_PER_FRAME])
+            weights.append(score//(break_ - prev_break + 1))
 
-        return audio_score
+        return weights
 
-    def _get_score(self, start: int, end: int) -> int:
-        """
-        :param start: start index of a short
-        :param end: end index of a shot
-        :return score
-        """
-        print(self.audio_data)
-        print(type(self.audio_data))
-        print(type(self.audio_data[0]))
-        return 0
