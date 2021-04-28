b4612d6e5f9c0ed1e66eddfd06216440747ac808
MarkStudio <markchen8024@gmail.com>
2021-04-28 16:55:54 +0800

    change parameters.

--- FullShotAnalyzer.py
+++ FullShotAnalyzer.py
@@ -8,24 +8,25 @@ from FaceDetector import FaceDetector
 from AudioAnalyzer import AudioAnalyzer
 
 class FullShotAnalyzer:
-    def __init__(self, data):
+    def __init__(self, data, break_points=None):
         self.data = data
         self.score = []
         '''
             For every n frames, calculae the total score in that step.
         '''
         self.step = 5
+        self.break_points = None
 
     def get_normalization(self, scores):
         min_score = min(scores)
         max_score = max(scores)
         return (scores - min_score) / float(max_score-min_score)
 
-    def get_weights_info(self, break_points, sum_per_shot):
+    def get_weights_info(self, sum_per_shot):
         prev_break = 0
         weights = []
-        for i in range(1, len(break_points)):
-            weights.append((sum_per_shot[i-1], break_points[i-1], break_points[i]))
+        for i in range(1, len(self.break_points)):
+            weights.append((sum_per_shot[i-1], self.break_points[i-1], self.break_points[i]))
 
         weights.sort(reverse=True)
 
@@ -55,8 +56,9 @@ class FullShotAnalyzer:
         '''
             Find the video break points
         '''
-        break_points = ShotsGenerator(self.data, 20).get_break_points()
-        print('Finished shot seperation ----------')
+        if self.break_points == None:
+            self.break_points = ShotsGenerator(self.data, 20).get_break_points()
+            print('Finished shot seperation ----------')
 
         '''
             Get motion scores for every shot.
@@ -65,14 +67,14 @@ class FullShotAnalyzer:
         end = self.data.frame_count
 
         motion_detector =  MotionDetector(self.data, self.step, 15)
-        motion_score = motion_detector.get_motion_score_per_shot(break_points, start, end)
+        motion_score = motion_detector.get_motion_score_per_shot(self.break_points, start, end)
         nor_motion_score = self.get_normalization(motion_score)
 
         faceDetector = FaceDetector(self.data, self.step)
-        face_score = faceDetector.get_face_score_per_shot(break_points)
+        face_score = faceDetector.get_face_score_per_shot(self.break_points)
         nor_face_score = self.get_normalization(face_score)
 
-        audio_analyzer = AudioAnalyzer(break_points, self.data)
+        audio_analyzer = AudioAnalyzer(self.break_points, self.data)
         audio_score = audio_analyzer.get_audio_score_per_shot()
         nor_audio_score = self.get_normalization(audio_score)
 
@@ -85,7 +87,7 @@ class FullShotAnalyzer:
 
         print(sum_per_shot)
 
-        weights = self.get_weights_info(break_points, sum_per_shot)
+        weights = self.get_weights_info(sum_per_shot)
 
         # Take only needed amounts of shots based on weights
         final_weight =  self.get_key_shots(weights)
