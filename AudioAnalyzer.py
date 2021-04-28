cab41d802e1a8100db3dae9472221d8b40d3f049
MarkStudio <markchen8024@gmail.com>
2021-04-28 16:54:08 +0800

    contribution from James

--- AudioAnalyzer.py
+++ AudioAnalyzer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class AudioAnalyzer:
     def __init__(self, breaks, data):
@@ -6,12 +7,25 @@ class AudioAnalyzer:
         self.data = data
 
     def get_audio_score_per_step(self, step):
-        scores = []
+        audio_scores = []
+        audio_diff_scores = []
+        offset = step // 2
+        for i in range(offset, self.data.frame_count, step):
+            step_score = []
+            for j in range(-(step // 2), math.ceil(step / 2), 1):
+                if i + j >= self.data.frame_count:
+                    break
+                audio = np.fromstring(self.data.load(i + j).audio, np.int16)
+                score = np.mean(np.absolute(audio)) / 2.0
+                step_score.append(score)
+            step_score = np.mean(np.array(step_score))
+            audio_scores.append(abs(step_score))
 
-        for i in range(self.data.frame_count):
-            scores.append(int(sum(self.data.load(i).audio)))
-
-        return np.array(scores)
+            if len(audio_scores) > 1:
+                audio_diff_scores.append(abs(audio_scores[-2] - audio_scores[-1]))
+            else:
+                audio_diff_scores.append(0)
+        return np.array(audio_diff_scores)
 
     def get_audio_score_per_shot(self):
         """
