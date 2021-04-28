1740b111d2949e9d2894962a22d784a607c89b63
zxcbvnmditto <chccheng@ucdavis.edu>
2021-04-28 00:34:18 -0700

    Add AudioDetector to SubShot and Fix Bugs

--- SubShotAnalyzer.py
+++ SubShotAnalyzer.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+from collections import deque
 
 from MotionDetector import MotionDetector
 from ShotsGenerator import ShotsGenerator
 from FaceDetector import FaceDetector
+from AudioDetector import AudioDetector
 
 class SubShotAnalyzer:
     def __init__(self, data):
@@ -55,18 +57,19 @@ class SubShotAnalyzer:
 
     def get_peak_frame(self, sum_per_step, break_points):
         peak = 0
-        max_score = 0
+        max_score = float("-inf")
         peak_frame = []
-        queue = break_points[1:]
+        queue = deque(break_points[1:])
         for i in range(len(sum_per_step)):
+            if sum_per_step[i] > max_score:
+                peak = i*self.step
+                max_score = sum_per_step[i]
             if i*self.step > queue[0]:
-                queue.pop(0)
+                queue.popleft()
                 peak_frame.append(peak)
                 peak = i*self.step
                 max_score = sum_per_step[i]
-            elif sum_per_step[i] > max_score:
-                peak = i*self.step
-                max_score = sum_per_step[i]
+        peak_frame.append(peak)
         return np.array(peak_frame)
 
     def extract_frames(self, peak_frame, frame_per_shot, break_points):
@@ -77,8 +80,8 @@ class SubShotAnalyzer:
             counter = 0
             left_p = 0
             right_p=0
-            lower_bound = break_points[i-1]
-            upper_bound = break_points[i]
+            lower_bound = break_points[i]
+            upper_bound = break_points[i+1]
             while frame_per_shot[i]>counter and total_frame <self.data.frame_count:
                 if peak-left_p >= lower_bound and not res_mask[peak-left_p]:
                     res_mask[peak-left_p] = True
@@ -90,7 +93,6 @@ class SubShotAnalyzer:
                 right_p+=1
                 if peak-left_p < lower_bound and peak+right_p > upper_bound: break
             total_frame+=counter
-
         return res_mask
 
     def run(self):
@@ -107,7 +109,7 @@ class SubShotAnalyzer:
         start = 0
         end = self.data.frame_count
 
-        motion_detector =  MotionDetector(self.data, self.step, 15)
+        motion_detector = MotionDetector(self.data, self.step, 15)
         motion_score = motion_detector.get_motion_score_per_step(start, end)
         nor_motion_score = self.get_normalization(motion_score)
 
@@ -115,10 +117,14 @@ class SubShotAnalyzer:
         face_score = faceDetector.get_face_score_per_step()
         nor_face_score = self.get_normalization(face_score)
 
+        audioDetector = AudioDetector(self.data, self.step)
+        audio_score = audioDetector.get_audio_score_per_step()
+        nor_audio_score = self.get_normalization(audio_score)
+
         '''
             score_per_step collect all kinds of score and give each feature a weight
         '''
-        score_per_step = [nor_motion_score * 0.6, nor_face_score*0.4]
+        score_per_step = [nor_motion_score * 0.5, nor_face_score*0.25, nor_audio_score*0.25]
         sum_per_step = [sum(x) for x in zip(*score_per_step)]
 
         '''
